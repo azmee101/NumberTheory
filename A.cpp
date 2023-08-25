dd2b624b980bef493ed71ad3ac2da3b2a6aa3af4
Azmee <iazmee101@gmail.com>
2023-08-25 21:38:11 +0600

    A.cpp added

---
+++ A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void run_case(){
+    int a, b; cin >> a >> b;
+    int GCD = __gcd(a, b);
+    int LCM = (a * b) / GCD;
+    cout << GCD << ' ' << LCM << '\n';
+    return;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        run_case();
+    }
+    return 0;
+}
