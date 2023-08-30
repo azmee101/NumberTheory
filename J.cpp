5f712268905a71c0df221d937266abbad79b7246
Azmee <iazmee101@gmail.com>
2023-08-31 01:02:40 +0600

    updated

---
+++ J.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e8+7;
+bitset<N> is_prime;
+vector<int> primes;
+
+void sieve(){
+    primes.push_back(2);
+    for(int i = 3; i * i < N; i++){
+        if(!is_prime[i]){
+            for(int j = i*i; j < N; j+=i){
+                is_prime[j] = true;
+            }
+        }
+    }
+
+    for(int i = 3; i <= N-7; i+=2){
+        if(!is_prime[i]){
+            primes.push_back(i);
+        }
+    }
+}
+
+
+void run_case(){
+    for(int i = 0; i < primes.size(); i+=100){
+        cout << primes[i] << '\n';
+    }
+    return;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    sieve();
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        run_case();
+    }
+    return 0;
+}
---
+++ K.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void run_case(){
+    ll n, m, l, r;
+    cin >> n >> m >> l >> r;
+    
+    ll strt, end, mn = (n / __gcd(m, n)) * m;
+    if(n / __gcd(n, m) > r / m){
+        cout << 0 << '\n';
+        return;
+    }
+    if(mn < l){
+        if(l % (mn) == 0) strt = l;
+        else{
+            ll x = l/mn + 1;
+            strt = mn * x;
+        }
+    }
+    else strt = mn;
+    end = mn * (r / mn);
+    
+    cout << ((end/mn) - (strt/mn)) + 1  << '\n';
+    return;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        run_case();
+    }
+    return 0;
+}
